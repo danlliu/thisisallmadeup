55b9ed857a173cc23f3b4cda1689cefa3554c850
danlliu <danlliu@umich.edu>
2020-10-16 23:45:24 -0400

    Some UI, still working

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let code_input = $('#code');
+let line_nums = $('#line-numbers');
+
+
+function codechange() {
+    console.log('change');
+    line_nums.empty();
+    let sz = code_input.val().split('\n').length;
+    for (let i = 1; i <= sz; ++i) {
+        line_nums.append(`${i}<br/>`);
+    }
+    line_nums.append(`<br>`);
+}
+
+code_input.on('scroll', function () {
+    line_nums.scrollTop($(this).scrollTop());
+});
+line_nums.on('scroll', function () {
+    code_input.scrollTop($(this).scrollTop());
+});
